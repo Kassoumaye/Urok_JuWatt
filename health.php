9816e1049aa0061d8481bb90e1e588fe43f46782
charles <charlesdiatta0@gmail.com>
2023-10-09 12:54:12 +0200

    init

---
+++ health.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UROK JUWAT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+
+    <style>
+        article.container,
+        p,
+        h2 {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+            margin-bottom: 10px;
+            line-height: 1.4;
+        }
+
+        img {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 50%;
+            float: left;
+            shape-outside: margin-box;
+        }
+    </style>
+
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <p><span>L'ASSOCIATION </span>UROK JUWATT</p>
+        </div>
+        <ul class="menu">
+            <li><a href="index.php">Accueil </a></li>
+            <li><a href="health.php">Santé </a></li>
+            <li><a href="formation.php">Education </a></li>
+            <li><a href="about.php">Culture </a></li>
+            <li><a href="economie.php">Economie </a></li>
+            <li><a href="record.php">Archives </a></li>
+            <li><a href="evidence.php">Témoignage </a></li>
+            <li><a href="Contact.php">Contact </a></li>
+            <li><a href="apport.php">Contribution Symbolique </a></li>
+            <li><a href="donate.php">Effectuer un Don </a></li><br>
+        </ul>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </header>
+
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>Liens Rapides</h3>
+                <a href="index.php"> <i class="fas fa-angle-right"></i> Accueil</a>
+                <a href="health.php"> <i class="fas fa-angle-right"></i> Santé</a>
+                <a href="formation.php"> <i class="fas fa-angle-right"></i> Education</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> Culture</a>
+                <a href="economy.php"> <i class="fas fa-angle-right"></i> Economie</a>
+                <a href="record.php"> <i class="fas fa-angle-right"></i> Archives</a>
+                <a href="evidence.php"> <i class="fas fa-angle-right"></i> Témoignages</a>
+                <a href="contact.php"> <i class="fas fa-angle-right"></i> Nous contacter</a>
+                <a href="apport.php"> <i class="fas fa-angle-right"></i> Contribution Symbolique</a>
+                <a href="donate.php"> <i class="fas fa-angle-right"></i> Effectuer un Don</a>
+            </div>
+
+            <div class="box">
+                <h3>Liens Supplémentaires</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i>Soumettez nous vos Questions ?</a>
+                <a href="#"> <i class="fas fa-angle-right"></i>A propos de Nous</a>
+                <a href="#"> <i class="fas fa-angle-right"></i>Politiques de Confidentialité</a>
+                <a href="#"> <i class="fas fa-angle-right"></i>Conditions d'utilisations</a>
+            </div>
+
+
+            <div class="box">
+                <h3>Informations de Contact</h3>
+                <a href="#"> <i class="fas fa-phone"></i>+22 33 824 04 68 </a>
+                <a href="#"> <i class="fas fa-envelope"></i>contact@urokjuwatt.org</a>
+                <a href="#"> <i class="fas fa-map"></i>Association UROK JUWATT - Siege social SICAP – Dieupeul 3, Villa Nº 2722-C</a>
+            </div>
+
+            <div class="box">
+                <h3>Nous Suivre</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit">
+            &copy; copyright @ 2024 par <span>UROK JUWATT</span> .Tous les droits sont rèservés!
+
+            <div class="credits">
+                Designed by <a href="https://bootstrapmade.com/">C T D</a>
+            </div>
+        </div>
+
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,382 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UROK JUWAT</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- swiper css link -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+  <!-- font awesome cdn link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="style.css">
+
+  <style>
+    article.container,
+    p,
+    h2 {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 30px;
+      border-radius: 10px;
+      box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+      margin-bottom: 10px;
+      line-height: 1.4;
+    }
+
+    img {
+      width: 200px;
+      height: 200px;
+      object-fit: cover;
+      border-radius: 50%;
+      float: left;
+      shape-outside: margin-box;
+    }
+  </style>
+
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <p><span>L'ASSOCIATION </span>UROK JUWATT</p>
+    </div>
+    <ul class="menu">
+      <li><a href="index.php">Accueil </a></li>
+      <li><a href="health.php">Santé </a></li>
+      <li><a href="formation.php">Education </a></li>
+      <li><a href="about.php">Culture </a></li>
+      <li><a href="economie.php">Economie </a></li>
+      <li><a href="record.php">Archives </a></li>
+      <li><a href="evidence.php">Témoignage </a></li>
+      <li><a href="Contact.php">Contact </a></li>
+      <li><a href="apport.php">Contribution Symbolique </a></li>
+      <li><a href="donate.php">Effectuer un Don </a></li><br>
+    </ul>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+  </header>
+
+  <br><br>
+  <br><br>
+  <br><br>
+
+  <section class="home">
+
+    <div class="home-slider">
+
+      <div class="w">
+
+        <div class="slide" style="background:url(images/cropped-P1050301-le-beau-fromager-géant.jpg) no-repeat">
+
+          <div class="content">
+            <span>explorer, visiter, naviguer</span>
+            <h3>Voyager à travers UROK JUWATT</h3>
+            <a href="formation.php" class="btn">Pour plus d'Amples Infos</a>
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+  </section>
+
+
+  <br><br>
+  <br><br>
+  <!-- <br><br>
+  <br><br>
+  <br><br> -->
+
+  <!-- <div class="bloc">
+        <video autoplay="autoplay" muted="" loop="infinite" src="vid.mp4" ></video>
+       </div> -->
+
+  <br><br>
+  <br><br>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col align-self-start">
+        <h1 class="card-body bg-warning">SITUATION GENERALE</h1>
+      </div>
+      <div class="col align-self-center">
+        <h1 class="card-body bg-primary">1. PLAN</h1>
+      </div>
+      <div class="col align-self-end">
+        <h2 class="card-body bg-info">2. PRESENTATION I</h2>
+      </div>
+      <div class="col align-self-end">
+        <h3 class="card-body bg-secondary">3. BILAN MORAL II</h3>
+      </div>
+      <div class="col align-self-end">
+        <h4 class="card-body bg-success">4. REALISATIONS III</h4>
+      </div>
+      <div class="col align-self-end">
+        <h5 class="card-body bg-danger">5. BILAN FINANCIER IV</h5><br>
+      </div>
+    </div>
+  </div>
+  <br><br>
+
+  <article class="container">
+    <img src="Images/images (2).jpg" alt="logo">
+    <h2 id="header">I- PRESENTATION : Le Peuple Awatt</h2>
+    <p>Le peuple awatt est situé dans la partie extrême sud-ouest du Sénégal, plus particulièrement dans la commune de Diembéring. La commune de Diembéring située en Casamance dans le département de Oussouye, à l’ouest de la région de Ziguinchor s’étend sur une superficie de 237 km2. Elle constitue l’extrémité du territoire Sénégalais dans la côte sud-ouest atlantique
+      frontalière avec la Guinée Bissau. JUWAT est le nom traditionnel de l’ensemble des villages que sont: Boucotte Diola, Bouyouye, Diembéring et Nikine.</p>
+  </article>
+
+  <!-- <a href="https://urokjuwatt.org/">Le Site UROK JUWATT</a> -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+
+        <div class="container text-center">
+          <div class="row justify-content-start">
+            <div class="col-6 bg-secondary">
+              <h3 class="DEMOGRAPHIE">DEMOGRAPHIE</h3>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-6 bg-warning">
+              <p class="h2">Estimation de la Population et des Ménages en 2021</p>
+
+            </div>
+          </div>
+          <br>
+          <table>
+            <tr>
+              <th scope="col">Localité</th>
+              <th scope="col">Nombre de Ménages en 2009 </th>
+              <th scope="col">Nombre de Ménages 2021 </th>
+              <th scope="col">Population en 2017 </th>
+              <th scope="col">Population en 2021 </th>
+            </tr>
+            <tr>
+              <th scope="row">Boucotte Diola</th>
+              <td> 78</td>
+              <td> 98</td>
+              <td> 950</td>
+              <td> 1600</td>
+            </tr>
+            <tr>
+              <th scope="row">Bouyouye</th>
+              <td> 25</td>
+              <td> 30</td>
+              <td> 305</td>
+              <td> 973</td>
+            </tr>
+            <tr>
+              <th scope="row">Diembering</th>
+              <td> 261</td>
+              <td> 302</td>
+              <td> 3180</td>
+              <td> 11420</td>
+            </tr>
+            <tr>
+              <th scope="row">Nikine</th>
+              <td> 29</td>
+              <td> 35</td>
+              <td> 353</td>
+              <td> 585</td>
+            </tr>
+          </table>
+
+        </div>
+      </div>
+    </div>
+    <br>
+
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="heading-section">FORMULAIRE DE CONTACT #0303</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="wrapper img" style="background-image: url(images/img.jpg);">
+              <div class="row">
+                <div class="col-md-9 col-lg-7">
+                  <div class="contact-wrap w-100 p-md-5 p-4">
+                    <h3 class="mb-4" style="color:aliceblue">Nous ContacteR</h3>
+                    <div id="form-message-warning" class="mb-4"></div>
+                    <div id="form-message-success" class="mb-4" style="color: aliceblue;">Votre Message a été envoyé avec Succès, Merci!</div>
+                    <form method="POST" id="contactForm" name="contactForm" class="contactForm">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="name">Mr</label>
+                            <input type="text" class="form-control" name="mr" id="mr" placeholder="Mr.">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="name">Mme</label>
+                            <input type="text" class="form-control" name="mme" id="mme" placeholder="Mme.">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="name">Nom</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="email">Prènom</label>
+                            <input type="email" class="form-control" name="prenom" id="prenom" placeholder="Prénom">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="email">Tél</label>
+                            <input type="email" class="form-control" name="tèl" id="tel" placeholder="Tél">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="email">Email Address</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="label" for="subject">Mot de Passe</label>
+                            <input type="text" class="form-control" name="md" id="md" placeholder="Mot de Passe">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="label" for="subject">Sujet</label>
+                            <input type="text" class="form-control" name="sujet" id="sujet" placeholder="Sujet">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="label" for="#">Message</label>
+                            <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="500 caractères maximum"></textarea>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="subject">Date de Naissance</label>
+                            <input type="date" class="form-control" name="date" id="date" placeholder="Votre Date de Naissance">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label class="label" for="subject">Votre CV</label>
+                            <input type="file" class="form-control" name="cv" id="cv" placeholder="Votre CV">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label class="label" for="subject">Votre dernier Diplome</label>
+                            <input type="text" class="form-control" name="diplome" id="diplome" placeholder="Votre Dernier Diplome">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input type="submit" value="VALIDER" class="btn btn-primary">
+                            <input type="submit" value="EFFACER" class="btn btn-danger">
+                            <div class="submitting"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- <footer id="footer">
+        <div class="container text-center">
+          <div class="copyright">
+            &copy; Copyright <strong><span>UROK JUWATT</span></strong>
+          </div>
+          
+        </div>
+      </footer> -->
+
+    <section class="footer">
+
+      <div class="box-container">
+
+        <div class="box">
+          <h3>Liens Rapides</h3>
+          <a href="index.php"> <i class="fas fa-angle-right"></i> Accueil</a>
+          <a href="health.php"> <i class="fas fa-angle-right"></i> Santé</a>
+          <a href="formation.php"> <i class="fas fa-angle-right"></i> Education</a>
+          <a href="about.php"> <i class="fas fa-angle-right"></i> Culture</a>
+          <a href="economy.php"> <i class="fas fa-angle-right"></i> Economie</a>
+          <a href="record.php"> <i class="fas fa-angle-right"></i> Archives</a>
+          <a href="evidence.php"> <i class="fas fa-angle-right"></i> Témoignages</a>
+          <a href="contact.php"> <i class="fas fa-angle-right"></i> Nous contacter</a>
+          <a href="apport.php"> <i class="fas fa-angle-right"></i> Contribution Symbolique</a>
+          <a href="donate.php"> <i class="fas fa-angle-right"></i> Effectuer un Don</a>
+        </div>
+
+        <div class="box">
+          <h3>Liens Supplémentaires</h3>
+          <a href="#"> <i class="fas fa-angle-right"></i>Soumettez nous vos Questions ?</a>
+          <a href="#"> <i class="fas fa-angle-right"></i>A propos de Nous</a>
+          <a href="#"> <i class="fas fa-angle-right"></i>Politiques de Confidentialité</a>
+          <a href="#"> <i class="fas fa-angle-right"></i>Conditions d'utilisations</a>
+        </div>
+
+
+        <div class="box">
+          <h3>Informations de Contact</h3>
+          <a href="#"> <i class="fas fa-phone"></i>+22 33 824 04 68 </a>
+          <a href="#"> <i class="fas fa-envelope"></i>contact@urokjuwatt.org</a>
+          <a href="#"> <i class="fas fa-map"></i>Association UROK JUWATT - Siege social SICAP – Dieupeul 3, Villa Nº 2722-C</a>
+        </div>
+
+        <div class="box">
+          <h3>Nous Suivre</h3>
+          <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+          <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+          <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+          <a href="#"> <i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+      </div>
+
+      <div class="credit">
+        &copy; copyright @ 2024 par <span>UROK JUWATT</span> .Tous les droits sont rèservés!
+
+        <div class="credits">
+          Designed by <a href="https://bootstrapmade.com/">C T D</a>
+        </div>
+      </div>
+
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
